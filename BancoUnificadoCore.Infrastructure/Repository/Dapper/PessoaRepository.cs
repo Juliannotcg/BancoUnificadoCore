b52b6c11395f31e98e09f3cbc00d313317325209
Julianno Garcia <juliannotcg@hotmail.com>
2018-03-06 14:14:57 -0300

    asd

--- BancoUnificadoCore.Infrastructure/Repository/Dapper/PessoaRepository.cs
+++ BancoUnificadoCore.Infrastructure/Repository/Dapper/PessoaRepository.cs
@@ -4,6 +4,7 @@ using BancoUnificadoCore.Domain.Queries;
 using BancoUnificadoCore.Domain.ValueObjects;
 using BancoUnificadoCore.Infrastructure.Context;
 using Dapper;
+using System;
 using System.Linq;
 
 namespace BancoUnificadoCore.Infrastructure.Repository.Dapper
@@ -20,15 +21,15 @@ namespace BancoUnificadoCore.Infrastructure.Repository.Dapper
         public bool PessoaExist(Documento documento)
         {
             string numeroDocumento = documento.NumeroDocumento;
-            GetPessoaResult getPessoaResult = new GetPessoaResult();
 
-            var ass = _context.Connection.Query<GetPessoaResult>("SELECT * FROM PesPessoa").FirstOrDefault();
+            var result = _context.Connection
+                      .Query<GetPessoaResult>("SELECT * FROM PesPessoa WHERE PesDocumento = @Documento", new { Documento = numeroDocumento })
+                      .FirstOrDefault();
 
-            //var query = from c in ass
-            //            where ass.Contains(numeroDocumento)
-            //            select c;
-
-            return false;
+            if (!String.IsNullOrEmpty(result.PesDocumento))
+                return true;
+            else
+                return false;
         }
 
         public void Save(Pessoa pessoa)

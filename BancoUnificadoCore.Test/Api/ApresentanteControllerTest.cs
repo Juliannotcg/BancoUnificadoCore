c9bd8e536d57cd5f32b605e85ba1dbf180901320
Érik Thiago <erik.sousa@outlook.com>
2018-04-05 11:40:31 -0300

    implementação do teste da API

--- BancoUnificadoCore.Test/Api/ApresentanteControllerTest.cs
+++ BancoUnificadoCore.Test/Api/ApresentanteControllerTest.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
+﻿using BancoUnificadoCore.Domain.Commands;
+using BancoUnificadoCore.Test.Helpers.Fakers;
+using Bogus;
+using Civil21.Obito.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BancoUnificadoCore.Test.Api
 {
     [TestClass]
-    public class ApresentanteControllerTest
+    public class ApresentanteControllerTest : BaseWebTest
     {
-        //[TestMethod]
+        private Faker<CommandCreateApresentante> apresentante;
+
+        [TestMethod]
+        public async Task ReturnCreated()
+        {
+            // Arrange
+            apresentante = ApresentanteCommandFaker.Gerar();
+
+            var apresentanteGerado = apresentante.Generate();
+
+            // Act
+            StringContent content = new StringContent(JsonConvert.SerializeObject(apresentanteGerado), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/orgaoEmissor", content);
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
 
     }
 }

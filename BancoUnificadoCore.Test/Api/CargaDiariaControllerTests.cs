fdac1b62f67f3191551bac56af509015c0e495e0
Érik Thiago <erik.sousa@outlook.com>
2018-04-05 14:57:20 -0300

    Implementação dos testes dos controllers que tem overbo post.

---
+++ BancoUnificadoCore.Test/Api/CargaDiariaControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using BancoUnificadoCore.Domain.Commands;
+using BancoUnificadoCore.Test.Helpers.Fakers;
+using Bogus;
+using Civil21.Obito.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoUnificadoCore.Test.Api
+{
+    [TestClass]
+    public class CargaDiariaControllerTests : BaseWebTest
+    {
+        private Faker<CommandCreateCargaDiaria> cargaDiaria;
+
+        [TestMethod]
+        public async Task ReturnCreated()
+        {
+            // Arrange
+            cargaDiaria = CargaDiariaCommandFaker.Gerar();
+
+            var carga = cargaDiaria.Generate();
+
+            // Act
+            StringContent content = new StringContent(JsonConvert.SerializeObject(carga), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("v1/cargaDiaria", content);
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
+
+    }
+}
---
+++ BancoUnificadoCore.Test/Api/DevedorControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using BancoUnificadoCore.Test.Helpers.Fakers;
+using Civil21.Obito.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoUnificadoCore.Test.Api
+{
+    [TestClass]
+    public class DevedorControllerTests : BaseWebTest
+    {
+        //TO DO: Implementar o command do devedor
+        //private Faker<CommandCreateDevedor> devedor;
+
+        [TestMethod]
+        public async Task ReturnCreated()
+        {
+            // Arrange
+            var devedor = DevedorCommandFaker.Gerar();
+
+            var command = devedor.Generate();
+
+            // Act
+            StringContent content = new StringContent(JsonConvert.SerializeObject(command), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("v1/devedor", content);
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
+    }
+}
